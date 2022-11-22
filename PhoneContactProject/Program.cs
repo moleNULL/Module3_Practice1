287c3a6575a6bdb260c0a606dd4f21d177859416
moleNULL <94998782+moleNULL@users.noreply.github.com>
2022-11-22 16:22:32 +0200

    Add logic for config.json

--- PhoneContactProject/Program.cs
+++ PhoneContactProject/Program.cs
@@ -2,9 +2,19 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine(" ");
+            try
+            {
+                Starter.Run();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception. {ex.Message}");
+            }
+
+            Console.Write("\nPress any key to continue . . .");
+            Console.ReadKey();
         }
     }
 }
---
+++ PhoneContactProject/Starter.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace PhoneContactProject
+{
+    internal class Starter
+    {
+        public static void Run()
+        {
+            CultureInfo[] info = new Config().GetCultureInfo();
+
+            foreach (var cultureInfo in info)
+            {
+                Console.WriteLine(cultureInfo);
+            }
+        }
+    }
+}
